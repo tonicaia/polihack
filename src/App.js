f42e949630b407004845fb22a4133a451703a469
flaviuvr <flaviu024@gmail.com>
2021-12-03 20:19:29 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Home from './components/Home';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        The deadliners
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+        </Routes>
       </header>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return(
+    <div>
+      The deadliners
+    </div>
+  )
+}
+
+export default Home;
